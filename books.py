12fdb65527132e7d7d7a4099b8c375485d230857
lotié <lotieruth7@gmail.com>
2026-05-07 11:02:03 +0000

    Ajout de la  gestion des livres

---
+++ books.py
@@ -0,0 +1,15 @@
+books = []
+
+def ajouter_livre(titre, auteur):
+    livre = {
+        "titre": titre,
+        "auteur": auteur,
+        "disponible": True
+    }
+    books.append(livre)
+    return livre
+
+
+def lister_livres():
+    return books
+
